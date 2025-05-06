b72e9c5e31287b0149e0fe61c7315b86dc6c9168
alexis <arobin9999@gmail.com>
2025-05-06 13:46:14 +0200

    avancée

--- start.py
+++ start.py
@@ -14,16 +14,31 @@ delta=0.1
 x=np.arange(xmin,xmax+delta,delta)
 y=np.arange(ymin,ymax+delta,delta)
 
-print(x)
-print(y)
+print("x: ", x)
+print("y: ", y)
 
 X,Y=np.meshgrid(x,y)
-print(X)
-print(Y)
+print("X: ", X, len(X))
+print("Y: ", Y, len(Y))
 Z=f(X,Y)
-print(Z)
+print("Z: ", Z)
 
-fig=plt.figure()
-ax=fig.add_subplot(111,projection='3d')
-ax.plot_surface(X,Y,Z,cmap='nipy_spectral_r')
-plt.show()
+def afficherNormal():
+    fig=plt.figure()
+    ax=fig.add_subplot(111,projection='3d')
+    ax.plot_surface(X,Y,Z,cmap='nipy_spectral_r')
+    plt.show()
+
+def afficherGroupeDeNiveau():
+    cont=plt.contour(X, Y, Z,levels=[0.1,0.3,0.5,1,1.5,2,2.5,3])
+    plt.clabel(cont)
+    plt.title('courbes de niveau ')
+    plt.show()
+
+def gradf(x, y):
+    df_dx = 4 * x * (x**2 - 1)
+    df_dy = 2 * y
+    return df_dx, df_dy
+
+def descente(xinit,yinit,pas,epsilon,nmax):
+    pass
