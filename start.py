3615c5f8e886c65cd49a53f4306fc52b8df116d8
alexis <arobin9999@gmail.com>
2025-05-06 13:16:52 +0200

    début start.py

--- start.py
+++ start.py
@@ -1,3 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+
+
+def f(x,y):
+    return (x**2-1)**2+y**2
+
+xmin=-2
+xmax=2
+ymin=-2
+ymax=2
+delta=0.1
+x=np.arange(xmin,xmax+delta,delta)
+y=np.arange(ymin,ymax+delta,delta)
+
+X,Y=np.meshgrid(x,y) 
+Z=f(X,Y)
